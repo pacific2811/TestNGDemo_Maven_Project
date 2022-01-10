afc094a2e3a5113b95c07ba74d9fe4900f15f76d
Prashant Dhakad <prashantdhakad2811@gmail.com>
2022-01-11 00:59:26 +0530

    First Commit

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+    @Test
+    void test1(){
+        System.out.println("Test1");
+
+    }
+    @Test
+    void test2(){
+        System.out.println("Test2");
+
+    }
+    @Test
+    void test3(){
+        System.out.println("Test3");
+
+    }
+}
